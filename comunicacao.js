1c00d397984ba61bdcd68c7dccefd7b64d587b21
Ian Filgueiras <109982573+Ian-Filgueiras@users.noreply.github.com>
2024-05-07 20:27:37 -0300

    Add files via upload

---
+++ comunicacao.js
@@ -0,0 +1,33 @@
+const IComunicacaoRepository = require("../interfaces/iComunicacao");
+const { Comunicacao } = require("../data/dbContext");
+
+class ComunicacaoRepository extends IComunicacaoRepository {
+    constructor() {
+      super();
+    }
+  
+    adicionarComunicacao = async (comunicacao) => {
+        const novaComunicacao = await Comunicacao.create({
+            text: comunicacao.text,
+            data_envio: comunicacao.data_envio,
+            idPorteiro: comunicacao.idPorteiro,
+            idCondomino: comunicacao.idCondomino,
+            mensagem_p_para_c: comunicacao.mensagem_p_para_c,
+        });
+    };
+  
+    get = async (nome) => {
+      let comunicacao = await Comunicacao.findOne({
+        where: { nome: nome },
+      });
+  
+      return comunicacao;
+    };
+  
+    getAll = async () => {
+      let comunicacoes = await Comunicacao.findAll({});
+      return comunicacoes;
+    };
+  }
+  
+  module.exports = ComunicacaoRepository;
